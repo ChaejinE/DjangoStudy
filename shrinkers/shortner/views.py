2b5f40c38c1eeb8ccc492316ff992804311f7c24
ChaejinE <joung6517@gmail.com>
2022-06-11 18:28:53 +0900

    basic class

--- shrinkers/shortner/views.py
+++ shrinkers/shortner/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+from shortner.models import Users
+from shortner.forms import RegisterForm
 
 # Create your views here.
+
+
+def index(request):
+    user = Users.objects.filter(username="admin").first()
+    email = user.email if user else "Anonymous"
+    print(email)
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated is False:
+        email = "Anonymous"
+        print(email)
+
+    return render(request, "base.html", {"welcome_msg": "Hello"})
+
+
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == "GET":
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = Users.objects.filter(pk=user_id).first()
+        return render(request, "base.html", {"user": user, "params": [abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = Users.objects.filter(pk=user_id).update(username=username)
+
+        return JsonResponse(dict(msg="You just reached with Post Method"))
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        msg = "올바르지 않은 데이터"
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            msg = "회원 가입 완료"
+        return render(request, "register.html", {"form": form, "msg": msg})
+    else:
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        msg = "가입되어 있지 않거나 로그인 정보가 잘못 되었습니다."
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                msg = "로그인 성공"
+                login(request, user)
+        
+        return render(request, "login.html", {"form": form, "msg": msg})
+
+    else:
+        form = AuthenticationForm()
+        return render(request, "login.html", {"form": form})
+        
+def logout_view(request):
+    logout(request)
+    return redirect("index_1")
+
+@login_required
+def list_view(request):
+    page = int(request.GET.get("p", 1))
+    users = Users.objects.all().order_by("-id")
+    paginator = Paginator(users, 10)
+    users = paginator.get_page(page)
+    
+    return render(request, "boards.html", {"users": users})
--- shrinkers/shrinkers/settings.py
+++ shrinkers/shrinkers/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shortner.apps.ShortnerConfig",
+    "debug_toolbar",
+    "django_seed",
 ]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+LOGIN_URL = "/login"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -48,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "shrinkers.urls"
@@ -55,7 +65,7 @@ ROOT_URLCONF = "shrinkers.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -89,16 +99,22 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
 TIME_ZONE = "UTC"
 
